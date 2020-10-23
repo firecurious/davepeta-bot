3221e1bedb6043877aa1e5b37ea9f87f34aa09d1
firecurious <firecurious@gmail.com>
2020-10-22 19:34:57 -0600

    fixed die roll

--- cogs/sburb.py
+++ cogs/sburb.py
@@ -15,15 +15,31 @@ class Sburb(commands.Cog):
     async def roll(self, ctx, dice : str):
         """write something like 3d20 to roll dice"""
 
-        channel = dice.message.channel
+        channel = ctx.message.channel
 
-        print(dice.message.content)
-        dice = dice.message.content.split(" ")[1]
+        print(ctx.message.content)
+        dice = ctx.message.content.split(" ")[1]
         print(dice)
 
         dice = [int(x) if x else 1 for x in dice.split("d")]
         numbers = [randint(1, dice[1]) for i in range(0, dice[0])]
         await channel.send("🎲 {0} = {1}".format(numbers, sum(numbers)))
+
+    @commands.command()
+    async def strife(self, action:str):
+        """aggrieve or whatever"""
+        chan = action.message.channel
+        action = action.message.content.split(" ")[1]
+
+        m = "this action is not implemented"
+
+        if action == "aggrieve":
+            m = "cost 2 damage 1, reuse ok"
+        if action == "ok":
+            m = "what"
+
+        await chan.send(m)
+            
 
     @commands.command()
     async def flip(self, ctx):
